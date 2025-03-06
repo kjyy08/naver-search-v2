1c22aaf8ae578e68dad1aa885868a158282b04b2
김주엽 <kjyy08@naver.com>
2025-03-07 00:50:02 +0900

    feat: jsp 연동 및 코드 리팩터링

--- src/main/java/controller/search/SearchController.java
+++ src/main/java/controller/search/SearchController.java
@@ -1,5 +1,7 @@
 package controller.search;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import exception.handler.CustomExceptionHandler;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,9 +10,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.dto.NaverAPIResultItem;
 import service.search.SearchService;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet("/")
+@WebServlet("/search")
 public class SearchController extends HttpServlet {
     private final SearchService searchService;
 
@@ -19,21 +23,30 @@ public class SearchController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
         String searchKeyword = request.getParameter("keyword");
 
         try {
-            // SearchService를 통해 결과를 가져옵니다.
             List<NaverAPIResultItem> naverAPIResultItems = searchService.searchByKeyword(searchKeyword);
-
-            // 검색 결과를 request에 추가합니다.
-            request.setAttribute("searchResults", naverAPIResultItems);
-            request.setAttribute("keyword", searchKeyword);
-
-            // 결과를 naver.jsp로 전달합니다.
-            request.getRequestDispatcher("/WEB-INF/views/naver.jsp").forward(request, response);
+            String json = createJson(naverAPIResultItems);
+            createResponse(response, json);
         } catch (Exception e) {
             handleErrorResponse(response, e);
+        }
+    }
+
+    private String createJson(List<NaverAPIResultItem> naverAPIResultItems) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(naverAPIResultItems);
+    }
+
+    private void createResponse(HttpServletResponse response, String json) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.print(json);
+            out.flush();
         }
     }
 
@@ -41,4 +54,3 @@ public class SearchController extends HttpServlet {
         CustomExceptionHandler.handleErrorResponse(response, e);
     }
 }
-
